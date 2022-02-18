49bce61ea9cacbd34736de733a4662f9da5d5d86
勝貴 佐藤 <190797@jc-21.jp>
2022-02-18 14:31:03 +0900

    プロジェクト ファイルを追加します。

---
+++ JetCoaster.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp4
+{
+    public class JetCoaster
+        {
+            public static int ride
+                (int k, int[] g, int r)
+            {
+                int[] aaa = g;
+                int[] newaaa = new int[g.Length];
+                int boa = 0;
+
+                int ret = 0;
+
+                for (int a = 0; a < r; a++)
+                {
+                    int count = 0;
+                    for (int b = 0; b < aaa.Length; b++)
+                    {
+                        int boac = boa;
+                        boa += aaa[b];
+                        count++;
+                        if (boa > k)
+                        {
+                            boa = boac;
+                            count--;
+                            break;
+                        }
+                    }
+                    for (int c = 0; c < count; c++)
+                    {
+                        int bff = aaa[0];
+                        Array.Copy(aaa, 1, newaaa, 0, aaa.Length - 1);
+                        newaaa[newaaa.Length - 1] = bff;
+
+                        aaa = newaaa;
+                    }
+                    ret += boa;
+                    boa = 0;
+                }
+                return ret;
+            }
+    }
+}
---
+++ kuriage.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp4
+{
+     public class kuriage
+    {
+        public static int kuriage10(int i)
+        {
+            int a = i;
+            int min = 1;
+            int aaa;
+
+
+
+            if (a < 0)
+            {
+                a = System.Math.Abs(a);
+                min = -1;
+            }
+
+
+
+            int b = a % 10;
+            int c = (a / 10) % 10;
+
+
+
+            if (b == 0)
+            {
+                aaa = a * min;
+                return aaa;
+            }
+
+
+
+            c = c + 1;
+            aaa = c * 10;
+            aaa = aaa * min;
+
+
+
+            return aaa;
+        }
+    }
+}
